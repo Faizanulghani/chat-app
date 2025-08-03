8574c8317a741191e997b8eb7beb37cc34c31228
Faizan Ul Ghani <faizanulghani1@gmail.com>
2025-08-03 18:37:32 +0500

    getCurrentChatters created in controller

--- backend/controller/userHandlerController.js
+++ backend/controller/userHandlerController.js
@@ -1,3 +1,4 @@
+const conversationModel = require("../models/conversationModel");
 const userModel = require("../models/userModel");
 
 exports.getUserBySearch = async (req, res) => {
@@ -21,6 +22,43 @@ exports.getUserBySearch = async (req, res) => {
       .select("-password")
       .select("email");
     res.json({ success: true, users });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+exports.getCurrentChatters = async (req, res) => {
+  try {
+    const currentUserId = req.user._id;
+
+    const currentChatters = await conversationModel
+      .find({ participants: currentUserId })
+      .sort({ updatedAt: -1 });
+
+    if (!currentChatters || currentChatters.length === 0) {
+      return res.status(200).json([]);
+    }
+
+    const participantsIds = currentChatters.reduce((ids, conversation) => {
+      const otherParticipants = conversation.participants.filter(
+        (id) => id.toString() !== currentUserId.toString()
+      );
+      return [...ids, ...otherParticipants];
+    }, []);
+
+    const uniqueParticipantIds = [
+      ...new Set(participantsIds.map((id) => id.toString())),
+    ];
+
+    const users = await userModel
+      .find({ _id: { $in: uniqueParticipantIds } })
+      .select("-password -email");
+
+    const sortedUsers = uniqueParticipantIds.map((id) =>
+      users.find((u) => u._id.toString() === id)
+    );
+
+    res.json({ success: true, users: sortedUsers });
   } catch (error) {
     res.status(500).json({ success: false, message: error.message });
   }
--- backend/route/userRoute.js
+++ backend/route/userRoute.js
@@ -1,8 +1,12 @@
 let express = require("express");
 const isLogin = require("../middleware/isLogin");
-const { getUserBySearch } = require("../controller/userHandlerController");
+const {
+  getUserBySearch,
+  getCurrentChatters,
+} = require("../controller/userHandlerController");
 let router = express.Router();
 
-router.get("/search",isLogin,getUserBySearch)
+router.get("/search", isLogin, getUserBySearch);
+router.get("/currentchatters", isLogin, getCurrentChatters);
 
 module.exports = router;
